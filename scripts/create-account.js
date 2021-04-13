6b5b864f335d2f4e5944e9881302ce48300d3223
Maksymilian Padalak <mpadalak@edu.cdv.pl>
2021-04-13 18:20:43 +0200

    Timeline is automatically sorting timeline elements

--- scripts/create-account.js
+++ scripts/create-account.js
@@ -277,6 +277,43 @@ const timelineElementCreator = (title, date, description) => {
   });
 };
 
+const sortTimelineElements = () => {
+  var list, i, switching, b, shouldSwitch;
+  list = document.querySelector(".timeline-list");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // start by saying: no switching is done:
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    // Loop through all list-items:
+    for (i = 0; i < (b.length - 1); i++) {
+      // start by saying there should be no switching:
+      shouldSwitch = false;
+      /* check if the next item should
+      switch place with the current item: */
+      
+      console.log(b[i].querySelector('h4').textContent.substring(0,4))
+      
+      if (Number(b[i].querySelector('h4').textContent.substring(0,4)) > Number(b[i + 1].querySelector('h4').textContent.substring(0,4))) {
+        
+        /* if next item is numerically
+        lower than current item, mark as a switch
+        and break the loop: */
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark the switch as done: */
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+}
+
 submitTimlineElementButton.addEventListener("click", () => {
   timelineElementCreator(
     newElementTitleInput.value,
@@ -295,6 +332,8 @@ submitTimlineElementButton.addEventListener("click", () => {
     ease: "none",
     opacity: 0,
   });
+  sortTimelineElements()
+  console.log(timelineElementsDataArray)
 });
 
 //fill in event info
