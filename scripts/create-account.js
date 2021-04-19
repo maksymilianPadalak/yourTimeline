f8468a5b4df4df5a7033552206037ccac43d59cd
Maksymilian Padalak <mpadalak@edu.cdv.pl>
2021-04-19 19:08:30 +0200

    Comments and modal inputs validation added

--- scripts/create-account.js
+++ scripts/create-account.js
@@ -1,10 +1,9 @@
+//Your Timeline is a single page application using gsap animations
+
 //START SCREEN
 
 const startScreen = document.querySelector(".start-screen-wrapper");
 const startBtn = document.querySelector(".start-btn");
-const createOrShowExemplary = document.querySelector(
-  ".create-or-show-example-wrapper"
-);
 const accountInfo = document.querySelector(".create-account-wrapper");
 
 const words = ["your memories", "your history", "Your Timeline"];
@@ -35,6 +34,10 @@ words.forEach((word) => {
 
 gsap.to(startBtn, { duration: 2, opacity: 1, delay: 10 });
 
+const createOrShowExemplary = document.querySelector(
+  ".create-or-show-example-wrapper"
+); //this is next view, that startBtn will take us to
+
 startBtn.addEventListener("click", () => {
   gsap.to(startScreen, { duration: 1, ease: "none", y: -700, display: "none" });
   gsap.to(createOrShowExemplary, {
@@ -47,9 +50,12 @@ startBtn.addEventListener("click", () => {
 });
 
 //CHOICE BETWEEN CREATE ACCOUNT OR SHOW EXEMPLARY NBA TIMELINE
+//It is named "createOrShowExemplary"
 
 const newAccountBtn = document.querySelector(".new-account-btn");
 const showExemplaryBtn = document.querySelector(".show-exemplary-btn");
+
+//If you choose to start Your Timeline journey, you will be taken to accountInfo view (by clilcking start Your Timeline journey Btn), which lets you create new accout
 
 newAccountBtn.addEventListener("click", () => {
   gsap.to(createOrShowExemplary, {
@@ -65,7 +71,32 @@ newAccountBtn.addEventListener("click", () => {
   });
 });
 
-//Creation and navigation in exemplary NBA TIMELINE
+//If you choose to show exemplary Timeline, you will be taken to exemplary NBA timeline (that uses data from external API) view (by clilcking start Your Timeline journey Btn)
+
+showExemplaryBtn.addEventListener("click", () => {
+  clearTimeline();
+  gsap.to(createOrShowExemplary, {
+    duration: 1,
+    display: "none",
+    ease: "none",
+    opacity: 0,
+  });
+
+  //due to style issue start and login background must be deactivated before showing any timeline
+
+  gsap.to(".start-login-wrapper", {
+    duration: 0,
+    display: "none",
+    ease: "none",
+    opacity: 0,
+  });
+
+  createNBATimeline();
+});
+
+//CREATION AND NAVIGATION IN EXEMPLARY NBA TIMELINE
+
+//function to handle any http Request
 
 async function sendHttpRequest(method, url, headers, data) {
   try {
@@ -88,42 +119,49 @@ async function sendHttpRequest(method, url, headers, data) {
   }
 }
 
+//function that fetches data from external API and creates timeline elements using functions, that are declered later in TIMELINE sector
+
 async function createNBATimeline() {
   try {
-    for (let i = 1; i <= 30; i++) {
+    //as an example this function loads 30 games, but it can work with any namber of elements
+    //as projects grows, this function will we changed, to load data from database and load timelines safed by users
 
-      const loadingPercent = document.getElementById('loading-percent')
+    for (let i = 1; i <= 30; i++) {
+      const loadingPercent = document.getElementById("loading-percent"); //fetching progress shown in percent
+
+      //loading animation
 
       gsap.to(".loading-screen-wrapper", {
         duration: 1,
         display: "flex",
         opacity: 1,
-        delay: 0.3
+        delay: 0.3,
       });
 
-      loadingPercent.textContent = `${Math.floor(i/30*100)}%`
+      //for every iteration percent number grows, until it reaches 100, when fetching is completed
 
+      loadingPercent.textContent = `${Math.floor((i / 30) * 100)}%`;
 
-      
       const responseData = await sendHttpRequest(
         "GET",
         `https://free-nba.p.rapidapi.com/games/${i}`,
         {
           "x-rapidapi-key":
             "1025f1cd61msh0f812df9d32f1b7p17ee61jsn13bb88e9e271",
-          "x-rapidapi-host": "free-nba.p.rapidapi.com", //That is my key, I don't want every user to generate a new one, it's just to simulate working with database
+          "x-rapidapi-host": "free-nba.p.rapidapi.com", //That is my key, I don't want every user to generate a new one, it's just to simulate working with JSON data
         }
       );
 
+      //variable that stores game winner
       //there are no draws in NBA. so there are only 2 cases
       const winnerTeam =
         +responseData.home_team_score > +responseData.visitor_team_score
           ? responseData.home_team.name
           : responseData.visitor_team.name;
 
-      
-
+      //timeline elements are clones of first element with changed data, so we have to generate first element separatly
       if (i === 1) {
+        //function declared in TIMELINE sector
         createFirstTimelineElement(
           responseData.home_team.name + " vs " + responseData.visitor_team.name,
           responseData.date.substring(0, 10),
@@ -132,6 +170,7 @@ async function createNBATimeline() {
           }. That's why we love NBA so much!`
         );
       } else {
+        //function declared in TIMELINE sector
         timelineElementCreator(
           responseData.home_team.name + " vs " + responseData.visitor_team.name,
           responseData.date.substring(0, 10),
@@ -142,6 +181,7 @@ async function createNBATimeline() {
       }
     }
 
+    //animation that hides loading screen when fetching is completed
     gsap.to(".loading-screen-wrapper", {
       duration: 1,
       display: "none",
@@ -153,36 +193,18 @@ async function createNBATimeline() {
       opacity: 1,
       delay: 1,
     });
-
   } catch (error) {
     console.log(error.message);
   }
 }
 
-showExemplaryBtn.addEventListener("click", () => {
-  clearTimeline()
-  gsap.to(createOrShowExemplary, {
-    duration: 1,
-    display: "none",
-    ease: "none",
-    opacity: 0,
-  });
-
-  //due to style issue start and login background must be deactivated before showing any timeline
-
-  gsap.to(".start-login-wrapper", {
-    duration: 0,
-    display: "none",
-    ease: "none",
-    opacity: 0,
-  });
-
-  createNBATimeline();
-});
-
+//navigation inside exemplary NBA timeline
+//create account option only occurs in exemplary timeline, it won't be visable, when user is already logged in
 const createAccountFromExemplaryTimelineBtn = document.querySelector(
   ".create-account-from-exemplary-timeline-btn"
 );
+
+//animation from exemplary Timeline view to create account view
 
 createAccountFromExemplaryTimelineBtn.addEventListener("click", () => {
   gsap.to(".timeline-window-wrapper", {
@@ -197,6 +219,8 @@ createAccountFromExemplaryTimelineBtn.addEventListener("click", () => {
     opacity: 1,
     delay: 1,
   });
+
+  //due to style issue start and login background must be deactivated before showing any timeline
   gsap.to(".start-login-wrapper", {
     duration: 0,
     display: "flex",
@@ -208,15 +232,19 @@ createAccountFromExemplaryTimelineBtn.addEventListener("click", () => {
 //CREATE ACCOUNT SCREEN
 
 const crateAccountBtn = document.querySelector(".create-account-btn");
+
 const goBackFromCreateAccoutBtn = document.querySelector(
   ".go-back-from-create-account-button"
-);
+); //takes you back to choice between creating account and showing exemplary NBA timeline
+
+//inputs
 const nameInput = document.querySelector(".name-input");
 const usernameInput = document.querySelector(".username-input");
 const birthInput = document.querySelector(".birth-input");
 const passwordInput = document.querySelector(".password-input");
 const confirmPasswordInput = document.querySelector(".confirm-password-input");
-const validationInfo = document.querySelector(".validation-info");
+
+const validationInfo = document.querySelector(".validation-info"); //text that shows if all inputs are valid
 
 class Account {
   constructor(name, username, dateOfBirth, password) {
@@ -230,10 +258,12 @@ class Account {
 
 //Validation
 
+//checks if given character is a letter
 function isCharacterALetter(char) {
   return /[a-zA-Z]/.test(char);
 }
 
+//password has to be at least 8 chars long, contain at least one upper letter and one digit
 const checkIfContainsDigit = (text) => {
   digits = [];
   for (char of text) {
@@ -262,6 +292,7 @@ const checkIfContainsUpperLetter = (text) => {
   }
 };
 
+//function that checks all validation conditions
 const passwordValidation = () => {
   if (passwordInput.value !== confirmPasswordInput.value) {
     validationInfo.textContent = "Passwords must match!";
@@ -281,6 +312,8 @@ const passwordValidation = () => {
   }
 };
 
+//name and username cannot be empty
+//trim is used in case of user accidentally uses space before or after their input
 const nameAndUsernameValidation = () => {
   if (
     nameInput.value.trim().length === 0 ||
@@ -295,20 +328,25 @@ const nameAndUsernameValidation = () => {
   }
 };
 
+//you cannot be born before 1903 and after current date in order to create account.
+//The oldest person in the world is 117-year-old Kane Tanaka of Japa and was born in 1903 (second of January)
 const dateValidation = () => {
   if (
-    Date.parse(birthInput.value) > new Date() || //new Date () return current date! :)
-    Date.parse(birthInput.value) < Date.parse("1900-01-01")
+    Date.parse(birthInput.value) > new Date() || //new Date () returns current date! :)
+    Date.parse(birthInput.value) < Date.parse("1903-01-02")
   ) {
     validationInfo.textContent =
       "You are immortal or come from a future ;) Enter valid date of birth!";
     validationInfo.style.color = "red";
     return false;
   } else {
-    return true
+    return true;
   }
 };
 
+//there is no database yet, there will be function, that checks if Username already exist added with it
+
+//takes you back to choice between creating account and showing exemplary NBA timeline
 goBackFromCreateAccoutBtn.addEventListener("click", () => {
   gsap.to(".create-account-wrapper", {
     duration: 1,
@@ -326,13 +364,14 @@ goBackFromCreateAccoutBtn.addEventListener("click", () => {
 });
 
 //global variables used to handle users data, and current user that is looged in
-const accounts = [];
-let usernamesArray = [];
 
+const accounts = []; //accounts will become useful, when there will be database with many users info
+let usernamesArray = []; //usernameArrays stores all usernames, and is used to compare username with password, when user tries to log in later
+
+//if all data is valid, when you click create account button, it will take you to logging in view
 crateAccountBtn.addEventListener("click", () => {
-  dateValidation();
   if (nameAndUsernameValidation() && passwordValidation() && dateValidation()) {
-    clearTimeline();
+    clearTimeline(); //If you opened exemplary NBA timeline before it has to be cleared, before displaying a new one. this function is declared in TIMELINE sector
     validationInfo.textContent = "All good! :)";
     validationInfo.style.color = "green";
     accounts.push(
@@ -343,6 +382,8 @@ crateAccountBtn.addEventListener("click", () => {
         passwordInput.value
       )
     );
+
+    //animation that takes you to logging in view
     gsap.to(".create-account-wrapper", {
       duration: 1,
       ease: "none",
@@ -358,8 +399,7 @@ crateAccountBtn.addEventListener("click", () => {
       delay: 1,
     });
 
-    //There is cuurently no way to go back, when you are alredy logged in. Log out function will be added soon!
-
+    //deactivates button, that lets you create account when seeing exemplary timeline, because you already have and account
     gsap.to(createAccountFromExemplaryTimelineBtn, {
       duration: 1,
       ease: "none",
@@ -369,17 +409,24 @@ crateAccountBtn.addEventListener("click", () => {
   } else {
     return;
   }
+
+  //adds all usernames to array, that is later udes to check if username exists and if password matches username, when logging in
   usernamesArray = accounts.map((account) => account.username);
 });
 
-//Login Screen//
+//LOGGING IN SCREEN//
 
+//inputs
 const loginUsername = document.querySelector(".username-login-input");
 const loginPassword = document.querySelector(".password-login-input");
+
 const loginBtn = document.querySelector(".login-button");
+
+//text, that shows if username doesn't exist or password is wrong
 const loginValidationText = document.querySelector(".login-validation-wrapper");
 let logedInUser;
 
+//checks if username exist in accounts array
 const userNameLoginValidation = () => {
   if (usernamesArray.includes(loginUsername.value.trim())) {
     return true;
@@ -389,6 +436,9 @@ const userNameLoginValidation = () => {
     return false;
   }
 };
+
+//returns username password
+//when actually using, it can't be visible to client side!!!
 
 const getPasswordFromUsername = () => {
   for (user of accounts) {
@@ -411,8 +461,10 @@ loginBtn.addEventListener("click", () => {
         }
       }
 
+      //info for developer, that right user is logged in
       console.log(`Welcome ${logedInUser.username}!`);
-      console.log("You are ready to log in!");
+
+      //animation, that takes us to TIMELINE view
       gsap.to(".login-wrapper", {
         duration: 1,
         ease: "none",
@@ -441,8 +493,8 @@ loginBtn.addEventListener("click", () => {
       loginValidationText.style.color = "green";
     }
   }
-  //Create first timeline element with name and date of birth of an User
-  //function createfirstTimelineElement is created later, to make every variable origin clear!
+
+  //function createfirstTimelineElement Creates first timeline element with name and date of birth of an User is declared in TIMELINE sector
   createFirstTimelineElement(
     `The day ${logedInUser.name} was born!`,
     logedInUser.dateOfBirth,
@@ -450,13 +502,14 @@ loginBtn.addEventListener("click", () => {
   );
 });
 
-//Timeline screen
+//TIMELINE
 const timeline = document.querySelector(".timeline-list");
 
 const createTimelineElementButton = document.querySelector(
   ".create-timeline-element-btn"
 );
 
+//visible in modal
 const submitTimlineElementButton = document.querySelector(
   ".submit-timline-element-btn"
 );
@@ -466,8 +519,9 @@ const newElementDescriptionInput = document.querySelector(
   ".event-description-input"
 );
 
-let timelineElementsDataArray = [];
+let timelineElementsDataArray = []; //array that stores all timeline elements, it will be useful when working with database, in order to safe users timelines
 
+//first timeline element is in original html script, other elements are just clones of the first one, with differente data, so we only have to change text content of the first one
 const createFirstTimelineElement = (title, date, description) => {
   const timelineElement = timeline.firstElementChild;
   timelineElement.querySelector("h1").textContent = title;
@@ -480,6 +534,7 @@ const createFirstTimelineElement = (title, date, description) => {
   });
 };
 
+//clones elements from first timeline element and fills then in with other text content
 const timelineElementCreator = (title, date, description) => {
   const timelineElement = timeline.firstElementChild.cloneNode(true);
   timelineElement.querySelector("h1").textContent = title;
@@ -494,9 +549,9 @@ const timelineElementCreator = (title, date, description) => {
 };
 
 //sorts Timeline Elements in order accoring to date
-
+//original function taken from w3schools.com, and modified, to work with Your Timeline web app
 const sortTimelineElements = () => {
-  var list, i, switching, b, shouldSwitch;
+  let list, i, switching, b, shouldSwitch;
   list = document.querySelector(".timeline-list");
   switching = true;
   /* Make a loop that will continue until
@@ -532,42 +587,23 @@ const sortTimelineElements = () => {
   }
 };
 
+//claers modal inputs every time, new element is added to Your Timeline
 const clearModalInputs = () => {
   (newElementTitleInput.value = ""),
     (newElementDateInput.value = ""),
     (newElementDescriptionInput.value = "");
 };
 
+//clears timeline and timelineElementsDataArray
 const clearTimeline = () => {
   const limit = timeline.childElementCount - 1;
-  for (let i = 0; i < limit; i++) timeline.firstElementChild.remove();
-  timelineElementsDataArray = []
+  for (let i = 0; i < limit; i++) {
+    timeline.firstElementChild.remove();
+  }
+  timelineElementsDataArray = [];
 };
 
-submitTimlineElementButton.addEventListener("click", () => {
-  timelineElementCreator(
-    newElementTitleInput.value,
-    newElementDateInput.value,
-    newElementDescriptionInput.value
-  );
-  clearModalInputs();
-  gsap.to(".modal-wrapper", {
-    duration: 0.5,
-    display: "none",
-    ease: "none",
-    opacity: 0,
-  });
-  gsap.to(".overlay", {
-    duration: 1,
-    display: "none",
-    ease: "none",
-    opacity: 0,
-  });
-  sortTimelineElements();
-  console.log(timelineElementsDataArray);
-});
-
-//fill in event info
+//MODAL (fill in event info for timeline element you want to add)
 
 const closeModalBtn = document.querySelector(".close-modal-button");
 
@@ -586,7 +622,7 @@ createTimelineElementButton.addEventListener("click", () => {
     ease: "none",
     opacity: 1,
   });
-  console.log(timelineElementsDataArray)
+  console.log(timelineElementsDataArray);
 });
 
 // close modal hanlder
@@ -604,4 +640,49 @@ closeModalBtn.addEventListener("click", () => {
     ease: "none",
     opacity: 0,
   });
+});
+
+const modalValidationText = document.getElementById("modal-inputs-validation"); //modal validation text
+
+//checks if no modal input is empty
+const modalInputsValidation = () => {
+  if (
+    newElementTitleInput.value.trim() === "" ||
+    newElementDescriptionInput.value.trim() === "" ||
+    newElementDateInput.value.trim() === ""
+  ) {
+    modalValidationText.style.color = "red";
+    modalValidationText.textContent =
+      "Fields cannot be empty :(!";
+    return false;
+  } else {
+    return true;
+  }
+};
+
+//creates new element from data provided by user closes modal and clearing inputs
+//only works if inputs aren't empty
+submitTimlineElementButton.addEventListener("click", () => {
+  if (modalInputsValidation()) {
+    timelineElementCreator(
+      newElementTitleInput.value,
+      newElementDateInput.value,
+      newElementDescriptionInput.value
+    );
+    clearModalInputs();
+    gsap.to(".modal-wrapper", {
+      duration: 0.5,
+      display: "none",
+      ease: "none",
+      opacity: 0,
+    });
+    gsap.to(".overlay", {
+      duration: 1,
+      display: "none",
+      ease: "none",
+      opacity: 0,
+    });
+    sortTimelineElements();
+    console.log(timelineElementsDataArray);
+  }
 });
